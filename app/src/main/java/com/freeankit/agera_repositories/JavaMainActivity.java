c809395c1ae8bc697a850e56ddb9a05599bb6bc1
AnkitDroidGit <urmailing2ankit@gmail.com>
2018-01-04 16:50:56 +0800

    added link to ComplexRepositoryActivity

--- app/src/main/java/com/freeankit/agera_repositories/JavaMainActivity.java
+++ app/src/main/java/com/freeankit/agera_repositories/JavaMainActivity.java
@@ -1,12 +1,9 @@
 package com.freeankit.agera_repositories;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.agera.MutableRepository;
